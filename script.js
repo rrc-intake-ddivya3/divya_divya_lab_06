8bf9cd4059e4fa1735d3086925a097540ed59ce4
divya sharma <sddivyasharma2005@gmail.com>
2025-11-07 23:41:16 -0600

    cookies function

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// script.js Divya Divya
+
+// function for cookies
+function setCookie(name, value, days) {
+    let expires = "";
+	if (days) {
+		const date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+	const value = "; " + document.cookie;
+	const parts = value.split("; " + name + "=");
+	if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+
+
+
+
+
+
+
 /**
  * Displays the top banner by removing the 'hide' class from it.
  * Uses a short delay to ensure the transition is triggered.
